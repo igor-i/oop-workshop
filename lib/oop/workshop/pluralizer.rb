bd972544d28430bf03302cab68dda54fa43f8f2a
igor-i <inkovskiy@gmail.com>
2018-09-15 10:20:01 +0300

    don't mutate the string, Luke

--- lib/oop/workshop/pluralizer.rb
+++ lib/oop/workshop/pluralizer.rb
@@ -4,7 +4,7 @@ module Oop
     module Pluralizer
       def self.plural(str, symbol = 's')
         return str if str.end_with? symbol
-        str << symbol
+        str.concat symbol
       end
     end
   end
